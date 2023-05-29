521f89e62c5149fc4196efcf714d12eed7c4c8c7
Your Name <youremail@yourdomain.com>
2023-05-30 00:29:40 +0700

    update sidebar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,25 +6,19 @@ import { RadioComponent } from './pages/radio/radio.component';
 import { CanhanComponent } from './pages/canhan/canhan.component';
 import { ZingchartComponent } from './pages/zingchart/zingchart.component';
 import { TheodoiComponent } from './pages/theodoi/theodoi.component';
-import { NhacmoiComponent } from './pages/nhacmoi/nhacmoi.component';
-import { TheloaiComponent } from './pages/theloai/theloai.component';
-import { Top100Component } from './pages/top100/top100.component';
-import { MvComponent } from './pages/mv/mv.component';
+import { HomeComponent } from './pages/home/home.component';
 
 
 const routes: Routes = [
   {
     path: '', component: BaseLayoutComponent, children: [
       { path: "", redirectTo: "home", pathMatch: "full" },
+      { path: "home", component: HomeComponent },
       { path: "canhan", component: CanhanComponent },
       { path: "khampha", component: KhamphaComponent },
       { path: "zingchart", component: ZingchartComponent },
       { path: "radio", component: RadioComponent },
       { path: "theodoi", component: TheodoiComponent },
-      { path: "nhacmoi", component: NhacmoiComponent },
-      { path: "theloai", component: TheloaiComponent },
-      { path: "top100", component: Top100Component },
-      { path: "mv", component: MvComponent },
     ]
   },
 
