de4fc8b3f40d22ef913a741fa64ddb9c94515ec0
Duy <duytv.dev@gmail.com>
2023-06-13 13:28:44 +0700

    fix : add music

--- src/app/pages/admin/add-product/add-product.component.ts
+++ src/app/pages/admin/add-product/add-product.component.ts
@@ -7,6 +7,7 @@ import { HttpClient } from '@angular/common/http';
   styleUrls: ['./add-product.component.scss']
 })
 export class AddProductComponent {
+  // fix add 
   product: any = {
     name: '',
     audio: '',
